8f73c80ab355ccf036715782442c35afb8f9f2bc
Nándor Krácser (nandork) <nandork@cisco.com>
2022-05-04 15:24:28 +0200

    register netfilter for all network namespaces (#2)
    * register netfilter for all network namespaces
    
    * mark client connection

--- worker_thread.c
+++ worker_thread.c
@@ -10,6 +10,7 @@
 
 static struct socket *sock;
 static struct socket *c_sock;
+static bool c_connected = false;
 
 static void accept_work(struct work_struct *);
 static int send_msg(struct socket *sock, char *);
@@ -26,7 +27,6 @@ static int make_server_socket(void)
 {
     int ret;
     struct sockaddr_un addr;
-    c_sock = NULL;
 
     // create
     ret = sock_create_kern(&init_net, AF_UNIX, SOCK_STREAM, 0, &sock);
@@ -70,6 +70,7 @@ static void accept_work(struct work_struct *dummy)
     else
     {
         printk(KERN_INFO "Accepted connection from on domain socket");
+        c_connected = true;
     }
 }
 
@@ -107,10 +108,11 @@ void submit_metric_handler(struct work_struct *work)
 
 void submit_metric(char *metric_line)
 {
-    printk("wasm3: submit_metric: %s, %d", metric_line, c_sock);
+    printk("wasm3: submit_metric: %s", metric_line);
 
-    if (!c_sock)
+    if (!c_connected)
     {
+        printk("wasm3: submit_metric: no clients, dropping metric");
         kfree(metric_line);
         return;
     }
