85b88bc0a012391281a0d80c0f1e481f0d126422
Nándor Krácser (nandork) <nandork@cisco.com>
2022-07-11 10:27:49 +0200

    Hastable: overwrite/update fix and implementation + iteration copy fix (#33)
    * hashtable: implement override and other fixes
    
    * keys_from_module_hashtable copy size fix

--- hashtable.c
+++ hashtable.c
@@ -21,32 +21,33 @@ void add_to_module_hashtable(i32 id, void* data, i32 data_length) {
     // Since linux hashtable always appends the new element to the bucket ignoring key collision altogether
     // we have to check whether an item with the key is already present in the map.
     // If it does we must handle that with raising an error.
-    struct h_node *cur;
-
-    struct h_node *possible_duplicate_data;
+    bool update = false;
+    struct h_node *cur = NULL;
+    struct h_node *bucket = NULL;
 
     hash_for_each_possible(module_table, cur, node, id) {
-        possible_duplicate_data = cur;
-    }
-    if (!possible_duplicate_data) {
-        printk("ERROR: Duplicate key found in hashmap, ignoring");
-        return;
+        bucket = cur;
     }
 
-    struct h_node *bucket = kmalloc(sizeof(struct h_node), GFP_KERNEL);
+    if (bucket) {
+        printk("wasm3: duplicate key (%d) found in hashmap, overwriting", id);
+        kfree(bucket->data);
+        update = true;
+    } else {
+        bucket = kzalloc(sizeof(struct h_node), GFP_KERNEL);
+    }
 
-    void *stored_data = (void *)kmalloc(data_length, GFP_KERNEL);
-
-    memcpy(stored_data, data, data_length);
-
-    bucket->data = stored_data;
     bucket->data_length = data_length;
     bucket->key = id;
+    bucket->data = (void *)kmalloc(data_length, GFP_KERNEL);
+    memcpy(bucket->data, data, data_length);
 
-    hash_add(module_table, &bucket->node, id);
-    //Since linux hashtable always appends the new element to the bucket ignoring key collision altogether
-    // so it is safe to increase our element tracker as well once the add function succeeds.
-    HASH_TABLE_ELEMENTS++;
+    if (!update) {
+        hash_add(module_table, &bucket->node, id);
+        // Since linux hashtable always appends the new element to the bucket ignoring key collision altogether
+        // so it is safe to increase our element tracker as well once the add function succeeds.
+        HASH_TABLE_ELEMENTS++;
+    }
 }
 
 // keys_from_module_hashtable function is not thread safe
@@ -57,8 +58,7 @@ void keys_from_module_hashtable(void** data, i32* data_length) {
     unsigned i;
     unsigned j = 0;
     hash_for_each(module_table, i, cur, node) {
-        keys[j] = cur->key;
-        j++;
+        keys[j++] = cur->key;
     }
 
     // Get the Runtime memory
@@ -66,7 +66,7 @@ void keys_from_module_hashtable(void** data, i32* data_length) {
 
     i32 wasm_mem_ptr = wasm_malloc(HASH_TABLE_ELEMENTS);
     void* hashmapKeys = mem + wasm_mem_ptr;
-    memcpy(hashmapKeys, keys, HASH_TABLE_ELEMENTS);
+    memcpy(hashmapKeys, keys, sizeof(int) * HASH_TABLE_ELEMENTS);
 
     *data = wasm_mem_ptr;
     *data_length = HASH_TABLE_ELEMENTS;
@@ -81,7 +81,8 @@ void get_from_module_hashtable(i32 id, void** data, i32* data_length) {
         temp = cur;
     }
 
-    if (!temp) {
+    if (!temp)
+    {
         return;
     }
 
@@ -100,19 +101,20 @@ void get_from_module_hashtable(i32 id, void** data, i32* data_length) {
 // delete_from_module_hashtable function is not thread safe
 void delete_from_module_hashtable(i32 id) {
     // Look up the h_node for the given id
-    struct h_node *cur;
-    struct h_node *temp;
+    struct h_node *cur = NULL;
+    struct h_node *temp = NULL;
     hash_for_each_possible(module_table, cur, node, id) {
         temp = cur;
     }
+
+    if (!temp)
+    {
+        return;
+    }
+
     hash_del(&temp->node);
     HASH_TABLE_ELEMENTS--;
 
     kfree(temp->data);
     kfree(temp);
 }
-
-
-
-
-
