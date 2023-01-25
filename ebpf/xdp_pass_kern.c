88e9322d3f9b6d81be481721dc611767a979f221
Nándor Krácser (nandork) <nandork@cisco.com>
2023-01-25 16:06:28 +0100

    update and fix XDP example (#44)
    * update and fix XDP example
    
    * simplify printouts

--- ebpf/xdp_pass_kern.c
+++ ebpf/xdp_pass_kern.c
@@ -4,19 +4,13 @@
 #include <linux/if_ether.h>
 #include <linux/sched.h>
 
-// struct {
-// 	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
-// 	__uint(max_entries, 32 * 32);
-// 	__type(key, __be16);
-// 	__type(value, __u64);
-// } xdp_stats_map SEC(".maps");
-
-struct bpf_map_def SEC("maps") xdp_stats_map = {
-    .type = BPF_MAP_TYPE_PERCPU_HASH,
-    .key_size = sizeof(__u16),
-    .value_size = sizeof(__u64),
-    .max_entries = 64,
-};
+struct
+{
+    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+    __uint(max_entries, 32 * 32);
+    __type(key, __be16);
+    __type(value, __u64);
+} xdp_stats_map SEC(".maps");
 
 SEC("xdp")
 int xdp_prog_simple(struct xdp_md *ctx)
@@ -26,8 +20,7 @@ int xdp_prog_simple(struct xdp_md *ctx)
 
     if (data + sizeof(struct ethhdr) > data_end)
     {
-        char buf[] = "Not an eth hdr";
-        bpf_trace_printk(buf, sizeof(buf));
+        bpf_printk("Not an eth hdr");
         return XDP_PASS;
     }
 
@@ -37,10 +30,8 @@ int xdp_prog_simple(struct xdp_md *ctx)
     __u64 *cnt = bpf_map_lookup_elem(&xdp_stats_map, &h_proto);
     if (cnt == NULL)
     {
-
         __u32 cpu = bpf_get_smp_processor_id();
-        char buf[] = "New protocol packet with type: 0x%04X on CPU: %d";
-        bpf_trace_printk(buf, sizeof(buf), h_proto, cpu);
+        bpf_printk("New protocol packet with type: 0x%04X on CPU: %d", h_proto, cpu);
 
         __u64 value = 0;
         bpf_map_update_elem(&xdp_stats_map, &h_proto, &value, BPF_ANY);
@@ -55,8 +46,7 @@ int xdp_prog_simple(struct xdp_md *ctx)
 
     if (h_proto == ETH_P_IP || h_proto == ETH_P_IPV6)
     {
-        char buf[] = "Hello %dth IPv4/6 packet from the eBPF/xdp World!";
-        bpf_trace_printk(buf, sizeof(buf), *cnt);
+        bpf_printk("Hello %d. IPv4/6 packet from the eBPF/xdp World!", *cnt);
     }
 
     return XDP_PASS;
