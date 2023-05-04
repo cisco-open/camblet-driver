b345491f236250d7d91b7282d6826df210d2081f
Nandor Kracser <bonifaido@gmail.com>
2023-05-04 13:57:08 +0200

    wip

--- device_driver.h
+++ device_driver.h
@@ -17,8 +17,7 @@
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char __user *, size_t,
-                            loff_t *);
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
 
 #define SUCCESS 0
 #define DEVICE_NAME "wasm"                 /* Dev name as it appears in /dev/devices   */
