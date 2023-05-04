1e1c61ef18d31f3f9b9b90af99ed99c4728faaa0
Nándor István Krácser <bonifaido@gmail.com>
2023-05-04 14:27:16 +0200

    Module entrypoint fixes (#6)
    * some module entrypoint invocation fixes

--- device_driver.h
+++ device_driver.h
@@ -17,11 +17,10 @@
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char __user *, size_t,
-                            loff_t *);
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
 
 #define SUCCESS 0
-#define DEVICE_NAME "wasm"                 /* Dev name as it appears in /proc/devices   */
+#define DEVICE_NAME "wasm"                 /* Dev name as it appears in /dev/devices   */
 #define DEVICE_BUFFER_SIZE 2 * 1024 * 1024 /* Max length of the message from the device */
 
 int chardev_init(void);
