5fdda77b1d444bc1f94a63cd6a53f9554c7d2d98
Balint Molnar <balintmolnar91@gmail.com>
2023-06-30 13:50:36 +0200

    Implement pem encoding for priv_key

--- socket.c
+++ socket.c
@@ -786,21 +786,12 @@ int wasm_socket_init(void)
 		printk("RSA keys are successfully generated.");
 	}
 
-	printk("Generating private exponent");
-	br_rsa_compute_privexp rsa_priv_exp_comp = br_rsa_compute_privexp_get_default();
-	unsigned char priv_exponent[256];
-	size_t priv_exponent_size = rsa_priv_exp_comp(priv_exponent, &rsa_priv, 3);
-	if (rsa_pub.nlen != priv_exponent_size) {
-		printk("Error happened during priv_exponent generation");
-	}
+	size_t pem_len = br_pem_encode(NULL, NULL, BR_RSA_KBUF_PRIV_SIZE(2048), "RSA PRIVATE KEY", 0);
 
-	printk("Private exponent generated");
+	void *pem = kmalloc(pem_len+1, GFP_KERNEL);
+	br_pem_encode(pem, raw_priv_key, BR_RSA_KBUF_PRIV_SIZE(2048), "RSA PRIVATE KEY", 0);
 
-	// void* priv_key_in_der = kmalloc(sizeof(void*), GFP_KERNEL);
-
-	// size_t enc_key_length = br_encode_rsa_raw_der(priv_key_in_der, rsa_priv, rsa_pub, priv_exponent, priv_exponent_size);
-	// printk("Private key encoded to raw der format");
-
+	printk("%s ", &pem[1]);
 
 	printk(KERN_INFO "WASM socket support loaded.");
 
