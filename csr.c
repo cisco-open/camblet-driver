02190ff168ad03b8cffbb726190e85317c0acfb4
Balint Molnar <balintmolnar91@gmail.com>
2023-08-08 10:41:01 +0200

    Add license headers for new files

--- csr.c
+++ csr.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023 Cisco and/or its affiliates. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT OR GPL-2.0-only
+ *
+ * Licensed under the MIT license <LICENSE.MIT or https://opensource.org/licenses/MIT> or the GPLv2 license
+ * <LICENSE.GPL or https://opensource.org/license/gpl-2-0>, at your option. This file may not be copied,
+ * modified, or distributed except according to those terms.
+ */
+
 #include "csr.h"
 
 typedef struct csr_module
