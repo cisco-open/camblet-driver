f8252846622210cb7548626dd6f695c18d6632c7
Zsolt Varga <zsolt.varga@console.hu>
2023-09-15 14:03:56 +0200

    retrieve signed x509 cert from agent

--- commands.h
+++ commands.h
@@ -12,6 +12,7 @@
 #define commands_h
 
 #include "task_context.h"
+#include "bearssl.h"
 
 #define COMMAND_TIMEOUT_SECONDS 1
 
@@ -21,12 +22,22 @@ typedef struct command_answer
     char *answer;
 } command_answer;
 
+typedef struct csr_sign_answer
+{
+    char *error;
+    br_x509_certificate *chain;
+    size_t chain_len;
+    br_x509_trust_anchor *trust_anchors;
+    size_t trust_anchors_len;
+} csr_sign_answer;
+
 void free_command_answer(command_answer *cmd_answer);
 
 command_answer *send_command(char *name, char *data, task_context *context);
 
 command_answer *send_accept_command(u16 port);
 command_answer *send_connect_command(u16 port);
+csr_sign_answer *send_csrsign_command(unsigned char *csr);
 
 // create a linked list for outgoing commands
 typedef struct command
--- rsa_tools.h
+++ rsa_tools.h
@@ -12,4 +12,4 @@
 
 int init_rnd_gen(void);
 uint32_t generate_rsa_keys(br_rsa_private_key *rsa_priv, br_rsa_public_key *rsa_pub);
-size_t encode_rsa_priv_key_to_der(unsigned char *der, br_rsa_private_key *rsa_priv, br_rsa_public_key *rsa_pub);
+int encode_rsa_priv_key_to_der(unsigned char *der, br_rsa_private_key *rsa_priv, br_rsa_public_key *rsa_pub);
