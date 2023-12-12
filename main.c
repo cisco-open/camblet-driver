f37d5ff305add890099b2684fb1086ebb595c423
Nandor Kracser <bonifaido@gmail.com>
2023-12-12 14:46:39 +0100

    add tls as module dependency

--- main.c
+++ main.c
@@ -29,6 +29,7 @@ MODULE_AUTHOR("Nasp Mainteiners <team@nasp.io>");
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_DESCRIPTION("NASP - Kernel Space Access Control for Zero Trust Networking");
 MODULE_VERSION("0.3.0");
+MODULE_SOFTDEP("pre: tls");
 
 static bool proxywasm_modules = false;
 module_param(proxywasm_modules, bool, 0644);
