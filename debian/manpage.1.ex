21f9ad93fc5131c0801b86afd58c93205828564d
Balazs Berta <balazs.berta.dev@gmail.com>
2024-01-18 17:13:43 +0100

    Camblet-kernel-module => Camblet-driver

--- debian/manpage.1.ex
+++ debian/manpage.1.ex
@@ -4,7 +4,7 @@
 .\" First parameter, NAME, should be all caps
 .\" Second parameter, SECTION, should be 1-8, maybe w/ subsection
 .\" other parameters are allowed: see man(7), man(1)
-.TH Camblet-kernel-module SECTION "November 14 2023"
+.TH Camblet-driver SECTION "November 14 2023"
 .\" Please adjust this date whenever revising the manpage.
 .\"
 .\" Some roff macros, for reference:
