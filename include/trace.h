3e3cb560185736de1d2d90bf3e42718e9e184631
Zsolt Varga <zsolt.varga@console.hu>
2024-04-30 15:11:12 +0200

    add macro block

--- include/trace.h
+++ include/trace.h
@@ -32,27 +32,29 @@ char *compose_log_message(const char *message, int n, ...);
 
 int send_trace(const tcp_connection_context *conn_ctx, const char *message, int log_level, int n, ...);
 
-#define trace_log(conn_ctx, message, log_level, n, ...)                 \
-    char *log_message = compose_log_message(message, n, ##__VA_ARGS__); \
-    if (!IS_ERR(log_message))                                           \
-    {                                                                   \
-        switch (log_level)                                              \
-        {                                                               \
-        case LOGLEVEL_ERR:                                              \
-            pr_err("%s", log_message);                                  \
-            break;                                                      \
-        case LOGLEVEL_WARNING:                                          \
-            pr_warn("%s", log_message);                                 \
-            break;                                                      \
-        case LOGLEVEL_INFO:                                             \
-            pr_info("%s", log_message);                                 \
-            break;                                                      \
-        case LOGLEVEL_DEBUG:                                            \
-            pr_debug("%s", log_message);                                \
-            break;                                                      \
-        }                                                               \
-        kfree(log_message);                                             \
-    }                                                                   \
+#define trace_log(conn_ctx, message, log_level, n, ...)                     \
+    {                                                                       \
+        char *log_message = compose_log_message(message, n, ##__VA_ARGS__); \
+        if (!IS_ERR(log_message))                                           \
+        {                                                                   \
+            switch (log_level)                                              \
+            {                                                               \
+            case LOGLEVEL_ERR:                                              \
+                pr_err("%s", log_message);                                  \
+                break;                                                      \
+            case LOGLEVEL_WARNING:                                          \
+                pr_warn("%s", log_message);                                 \
+                break;                                                      \
+            case LOGLEVEL_INFO:                                             \
+                pr_info("%s", log_message);                                 \
+                break;                                                      \
+            case LOGLEVEL_DEBUG:                                            \
+                pr_debug("%s", log_message);                                \
+                break;                                                      \
+            }                                                               \
+            kfree(log_message);                                             \
+        }                                                                   \
+    }                                                                       \
     send_trace(conn_ctx, message, log_level, n, ##__VA_ARGS__);
 
 #define trace_err(conn_ctx, message, n, ...) \
